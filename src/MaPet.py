12c2a975e25752fe8ee958cd5f00ddd1aa50ed97
Bogdan Bucur <bogdan.bucur@udevoffice.ro>
2017-06-22 18:31:46 +0300

    Register and Login with Logout until now

--- src/MaPet.py
+++ src/MaPet.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import random
+from random import randint
 from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.touch_actions import TouchActions
@@ -11,18 +13,79 @@ class MaPetTest(unittest.TestCase):
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723/wd/hub',
             desired_capabilities={
-                'app': '/Users/bogdanbucur/PycharmProjects/MaPetTest/src/MaPet.ipa',
+                'app': '/Users/bogdanbucur/PycharmProjects/MaPetTest/src/MaPet.app',
+                'automationName': 'XCUITest',
                 'platformName': 'iOS',
-                'platformVersion': '10.0',
-                'deviceName': 'iPhone 6'
+                'platformVersion': '9.3',
+                'deviceName': 'iPhone 6s Plus'
             })
 
-    def test_1(self):
+    # # Register
+    # def test_1(self):
+    #     sleep(2)
+    #     if self.driver.find_element_by_accessibility_id('OK').is_displayed():
+    #         self.driver.find_element_by_accessibility_id('OK').click()
+    #     sleep(2)
+    #
+    #     self.driver.find_element_by_accessibility_id('REGISTER').click()
+    #
+    #     # Input Email Address
+    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').click()
+    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').clear()
+    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').send_keys('bogdan.bucur@udevoffice.ro')
+    #
+    #     # Input Password
+    #     passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
+    #     passField.click()
+    #     passField.clear()
+    #     passField.send_keys('supersecret')
+    #
+    #     # Confirm Password
+    #     confirmPassField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[1]
+    #     confirmPassField.click()
+    #     confirmPassField.clear()
+    #     confirmPassField.send_keys('supersecret')
+    #     self.driver.find_element_by_accessibility_id('Done').click()
+    #
+    #     # Submit
+    #     self.driver.find_element_by_accessibility_id('CONTINUE').click()
+    #     sleep(4)
+
+    # Login with Logout
+    def test_2(self):
         sleep(2)
+        if self.driver.find_element_by_accessibility_id('OK').is_displayed():
+            self.driver.find_element_by_accessibility_id('OK').click()
+        sleep(2)
+
+        self.driver.find_element_by_accessibility_id('LOGIN').click()
+        sleep(2)
+
+        # Input Email Address
+        emailField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')
+        emailField.click()
+        emailField.clear()
+        emailField.send_keys('bogdan.bucur@udevoffice.ro')
+
+        # Input Password
+        passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
+        passField.click()
+        passField.clear()
+        passField.send_keys('supersecret')
+
+        # Login
+        self.driver.find_element_by_accessibility_id('CONTINUE').click()
+        sleep(2)
+
+        # Logout
+        self.driver.find_element_by_accessibility_id('LOGOUT').click()
+        sleep(1)
+        self.driver.find_element_by_accessibility_id('Yes')
 
     def tearDown(self):
         self.driver.quit()
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(MaPetTest)
-unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(MaPetTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
