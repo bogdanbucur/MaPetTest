346724da8dbb7bde7ec97ecfdc707269a9329c94
Bogdan Bucur <bogdan.bucur@udevoffice.ro>
2017-06-23 18:15:55 +0300

    Bag pl

--- src/MaPet.py
+++ src/MaPet.py
@@ -17,70 +17,139 @@ class MaPetTest(unittest.TestCase):
                 'automationName': 'XCUITest',
                 'platformName': 'iOS',
                 'platformVersion': '9.3',
-                'deviceName': 'iPhone 6s Plus'
+                'deviceName': 'iPhone 6s Plus',
+                'noReset': 'True'
             })
 
-    # # Register
-    # def test_1(self):
-    #     sleep(2)
-    #     if self.driver.find_element_by_accessibility_id('OK').is_displayed():
-    #         self.driver.find_element_by_accessibility_id('OK').click()
-    #     sleep(2)
-    #
-    #     self.driver.find_element_by_accessibility_id('REGISTER').click()
-    #
-    #     # Input Email Address
-    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').click()
-    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').clear()
-    #     self.driver.find_element_by_class_name('XCUIElementTypeTextField').send_keys('bogdan.bucur@udevoffice.ro')
-    #
-    #     # Input Password
-    #     passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
-    #     passField.click()
-    #     passField.clear()
-    #     passField.send_keys('supersecret')
-    #
-    #     # Confirm Password
-    #     confirmPassField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[1]
-    #     confirmPassField.click()
-    #     confirmPassField.clear()
-    #     confirmPassField.send_keys('supersecret')
-    #     self.driver.find_element_by_accessibility_id('Done').click()
-    #
-    #     # Submit
-    #     self.driver.find_element_by_accessibility_id('CONTINUE').click()
-    #     sleep(4)
+#   Register
+#     def test_1(self):
+#         # Allow notifications
+#         sleep(2)
+#         if self.driver.find_element_by_accessibility_id('OK').is_displayed():
+#             self.driver.find_element_by_accessibility_id('OK').click()
+#         sleep(2)
+#
+#         self.driver.find_element_by_accessibility_id('REGISTER').click()
+#
+#         # Input Email Address
+#         self.driver.find_element_by_class_name('XCUIElementTypeTextField').click()
+#         self.driver.find_element_by_class_name('XCUIElementTypeTextField').clear()
+#         self.driver.find_element_by_class_name('XCUIElementTypeTextField').send_keys('bogdan.bucur@udevoffice.ro')
+#
+#         # Input Password
+#         passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
+#         passField.click()
+#         passField.clear()
+#         passField.send_keys('supersecret')
+#
+#         # Confirm Password
+#         confirmPassField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[1]
+#         confirmPassField.click()
+#         confirmPassField.clear()
+#         confirmPassField.send_keys('supersecret')
+#         self.driver.find_element_by_accessibility_id('Done').click()
+#
+#         # Submit
+#         self.driver.find_element_by_accessibility_id('CONTINUE').click()
+#         sleep(4)
 
-    # Login with Logout
-    def test_2(self):
+# Login with Logout
+#     def test_2(self):
+#         # Allow notifications
+#         sleep(2)
+#         if self.driver.find_element_by_accessibility_id('OK').is_displayed():
+#             self.driver.find_element_by_accessibility_id('OK').click()
+#         sleep(2)
+#
+#         self.driver.find_element_by_accessibility_id('LOGIN').click()
+#         sleep(2)
+#
+#         # Input Email Address
+#         emailField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')
+#         emailField.click()
+#         emailField.clear()
+#         emailField.send_keys('bogdan.bucur@udevoffice.ro')
+#
+#         # Input Password
+#         passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
+#         passField.click()
+#         passField.clear()
+#         passField.send_keys('supersecret')
+#
+#         # Hide Keyboard
+#         self.driver.find_element_by_accessibility_id('Done').click()
+#
+#         # Login
+#         self.driver.find_element_by_accessibility_id('CONTINUE').click()
+#         sleep(2)
+#
+#         # Logout
+#         self.driver.find_element_by_accessibility_id('LOGOUT').click()
+#         sleep(1)
+#         self.driver.find_element_by_accessibility_id('Yes')
+
+# Login
+#     def test_3(self):
+#         # Enter Login Page
+#         sleep(2)
+#         self.driver.find_element_by_accessibility_id('LOGIN').click()
+#         sleep(2)
+#
+#         # Input Email Address
+#         emailField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')
+#         emailField.click()
+#         emailField.clear()
+#         emailField.send_keys('bogdan.bucur@udevoffice.ro')
+#
+#         # Input Password
+#         passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
+#         passField.click()
+#         passField.clear()
+#         passField.send_keys('supersecret')
+#
+#         # Hide Keyboard
+#         self.driver.find_element_by_accessibility_id('Done').click()
+#
+#         # Login
+#         self.driver.find_element_by_accessibility_id('CONTINUE').click()
+#         sleep(2)
+
+#   Something
+    def test_4(self):
         sleep(2)
-        if self.driver.find_element_by_accessibility_id('OK').is_displayed():
-            self.driver.find_element_by_accessibility_id('OK').click()
+
+        # Input Name
+        self.driver.find_element_by_accessibility_id('ADD PET').click()
         sleep(2)
 
-        self.driver.find_element_by_accessibility_id('LOGIN').click()
+        nameField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')
+        nameField.click()
+        nameField.send_keys('Mr. Fluff')
+        self.driver.find_element_by_accessibility_id('Done').click()
+
+        # Add Custom Species
+        self.driver.find_element_by_accessibility_id('Species:').click()
         sleep(2)
-
-        # Input Email Address
-        emailField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')
-        emailField.click()
-        emailField.clear()
-        emailField.send_keys('bogdan.bucur@udevoffice.ro')
-
-        # Input Password
-        passField = self.driver.find_elements_by_class_name('XCUIElementTypeSecureTextField')[0]
-        passField.click()
-        passField.clear()
-        passField.send_keys('supersecret')
-
-        # Login
-        self.driver.find_element_by_accessibility_id('CONTINUE').click()
-        sleep(2)
-
-        # Logout
-        self.driver.find_element_by_accessibility_id('LOGOUT').click()
+        self.driver.find_element_by_accessibility_id('Done').click()
         sleep(1)
-        self.driver.find_element_by_accessibility_id('Yes')
+        specieField = self.driver.find_element_by_class_name('XCUIElementTypeTextField')[1]
+        specieField.click()
+        specieField.send_keys('Fluff')
+        self.driver.find_element_by_accessibility_id('Confirm').click()
+
+        # Select Male Sex
+        self.driver.find_element_by_accessibility_id('Sex (optional):').click()
+        sleep(1)
+        self.driver.find_element_by_accessibility_id('Done').click()
+
+        # Input Description
+        descriptionField = self.driver.find_element_by_class_name('XCUIElementTypeTextView')
+        descriptionField.click()
+        descriptionField.send_keys('Fluffiest fluff in the entire Fluff Land.')
+        self.driver.find_element_by_accessibility_id('Done').click()
+
+        # Create Pet
+        self.driver.find_element_by_accessibility_id('CREATE PET').click()
 
     def tearDown(self):
         self.driver.quit()
